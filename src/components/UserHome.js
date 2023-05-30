bef9ebfadc0cd9dde613b008bc9ed61e189d5594
grantferowich <g.ferowich@gmail.com>
2023-05-30 10:07:30 -0500

    optimized first name rendering

--- src/components/UserHome.js
+++ src/components/UserHome.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import axios from 'axios';
+import { useSelector } from "react-redux";
 
-
+// TO-DO as of 10:00 am
+// get current user id
+// get current user name
 // greet this particular user by name
 // render basket data via cards
 
 export default function UserHome() {
+  const user_IdStr = useSelector(state => state.user.id)
+  let user_firstNameStr = useSelector(state =>state.user.name)
   
+  if (user_firstNameStr.split(' ').length > 1){
+    user_firstNameStr = user_firstNameStr.split(" ")[0]
+  }
+  console.log('userIdStr', user_IdStr)
+  console.log('user_firstName', user_firstNameStr)
   let getBasketsAPIStr = 'https://epistemica-x-db.vercel.app/api/basket/getAll'
   let basketsArr = []
   const consumeAPI = (data) => {
       basketsArr = data;
-      console.log('Baskets Arr', basketsArr)
+
   }
   axios.get(getBasketsAPIStr)
   .then((responseHM) => {consumeAPI(responseHM.data)})
@@ -19,7 +29,7 @@ export default function UserHome() {
 
   return (
     <div>
-        <h2>Welcome home to Epistemica-X!</h2>
+        <h2>Welcome home to Epistemica-X, {user_firstNameStr}!</h2>
         
     </div>
   )
