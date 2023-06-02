b63d8ea07e275c39c34271fd23c2e2607bb2c06c
grantferowich <g.ferowich@gmail.com>
2023-06-02 07:53:48 -0500

    successfully accessed asset name and api keys in an array

--- src/containers/BasketCardContainer.js
+++ src/containers/BasketCardContainer.js
@@ -8,10 +8,11 @@ import { useSelector } from 'react-redux';
 
 export default function BasketCardContainer() {
   const basketsArr = useSelector(state => state.user.basketsArr);
-  let uniqueAssetsArr = [] ;
-
+  
+  console.log(basketsArr)
 
   const generateUniqueAssetArray = (basketsArr) => {
+    let uniqueAssetsArr = [];
     let xInt = 0
     while (xInt < basketsArr.length) {
       let basketHM = basketsArr[xInt]
@@ -21,31 +22,33 @@ export default function BasketCardContainer() {
       let asset4HM = basketHM.asset4HM;
       let asset5HM = basketHM.asset5HM;
       let originalAssetsArr = [asset1HM, asset2HM, asset3HM, asset4HM, asset5HM];
-      // const newAssetsArr = originalAssetsArr
-      // .flatMap(assetObj => {
-      //   let assetsArr = [];
-      //   let iInt = 1;
-      //   while (iInt <= 5){
-      //     let assetName = assetObj[`asset${iInt}NameStr`];
-      //     let apiKey = assetObj[`asset${iInt}APIKeyStr`];
-      //     if (assetName && !assetsArr.some(a => a.assetNameStr === assetName)){
-      //       assetsArr.push({assetNameStr: assetName, apiKeyStr: apiKey})
-      //     }
-      //     iInt++
-      //   }
-      //   return assetsArr
-      // })
-      console.log('uniqueAssetsArray', newAssetsArr);
+      const newAssetsArr = originalAssetsArr
+      .flatMap(assetObj => {
+        let assetsArr = [];
+        let iInt = 1;
+        while (iInt <= 5){
+          let assetName = assetObj[`asset${iInt}NameStr`];
+          let apiKey = assetObj[`asset${iInt}APIKeyStr`];
+          if (assetName && !assetsArr.some(a => a.assetNameStr === assetName)){
+            assetsArr.push({assetNameStr: assetName, apiKeyStr: apiKey})
+          }
+          iInt++
+        }  
+        return assetsArr;
+      });
+      uniqueAssetsArr = [...uniqueAssetsArr, newAssetsArr]
+      xInt++
     }
+    return uniqueAssetsArr;
   }
 
-  generateUniqueAssetArray(basketsArr);
-
+  const assetsArray = generateUniqueAssetArray(basketsArr);
+  console.log('assetsArray', assetsArray)
    
     return ( basketsArr.length !== 0 ?
     (<div>
         <div> 
-          <Grid container="true" display="flex" flexWrap="wrap" xs={12}>
+          <Grid container spacing={2}>
             {basketsArr.map(basket => {
               return (
                 <Grid item xs={12} sm={6} key={basket._id}>
