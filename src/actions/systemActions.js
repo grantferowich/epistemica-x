4b551359d179fa1b772fe68387707b139bc9f08e
grantferowich <g.ferowich@gmail.com>
2023-06-03 17:39:29 -0500

    documented system actions for coin list

--- src/actions/systemActions.js
+++ src/actions/systemActions.js
@@ -1,6 +1,12 @@
 export const SET_LAST_API_CALL_TIME = 'SET_LAST_API_CALL_TIME';
+export const SET_COIN_LIST = 'SET_COIN_LIST';
 
 export const setLastAPICallTime = (lastAPICallTime) => ({
     type: SET_LAST_API_CALL_TIME,
     payload: lastAPICallTime
+})
+
+export const setCoinList = (coinList) => ({
+    type: SET_COIN_LIST,
+    payload: coinList
 })
