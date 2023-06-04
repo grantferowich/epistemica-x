7cf81229957caa72ffbc282e6c933d77f0932575
grantferowich <g.ferowich@gmail.com>
2023-06-04 13:44:23 -0500

    created /create-basket route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from "./components/Login";
 import HomePage from "./components/HomePage";
 import SignUp from "./components/SignUp";
 import UserHome from "./components/UserHome";
+import BasketForm from "./components/BasketForm"
 import { Provider } from "react-redux";
 import store from './store/store';
 
@@ -19,6 +20,7 @@ function App() {
             <Route exact path="/login" element={<Login />}/>
             <Route exact path='/signup' element={<SignUp/>}/>
             <Route exact path='/user-home' element={<UserHome/>}/>
+            <Route exact path='/create-basket' element={<BasketForm/>}/>
           </Routes>
         </div>
       </Router>
--- src/store/store.js
+++ src/store/store.js
@@ -3,15 +3,21 @@ import { legacy_createStore as createStore} from 'redux'
 import { combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { Provider } from 'react-redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
+import { loadState, saveState } from './sessionStorage';
 
 import userReducer from '../reducers/useReducer';
+
+const persistedState = loadState();
 const composedEnhancer = applyMiddleware(thunkMiddleware)
 
 const rootReducer = combineReducers({
     user: userReducer
 })
 
-const store = createStore(rootReducer, composedEnhancer);
+const store = createStore(rootReducer, persistedState, composedEnhancer);
+
+store.subscribe(() => {
+    saveState(store.getState());
+})
 
 export default store;
