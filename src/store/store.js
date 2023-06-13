0cc37f577171099e05fdffe431e78ce7000143b2
grantferowich <g.ferowich@gmail.com>
2023-06-13 13:58:46 -0500

    set up redux sagas on store.js

--- src/store/store.js
+++ src/store/store.js
@@ -4,15 +4,14 @@ import { combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import userReducer from '../reducers/userReducer';
 import authReducer from '../reducers/authReducer';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './sagas.js'
 
-// const persistedState = loadState();
-const composedEnhancer = applyMiddleware(thunkMiddleware)
-
+const sagaMiddleware = createSagaMiddleware();
 const rootReducer = combineReducers({
     user: userReducer,
     auth: authReducer
 })
-
-const store = createStore(rootReducer, composedEnhancer);
-
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(rootSaga)
 export default store;
