c50f5ca090993a4e72cdf3b9d4e271104e6638f0
grantferowich <g.ferowich@gmail.com>
2023-06-28 12:23:59 -0500

    appended the card to the view-single-basket-page

--- src/components/ViewSingleBasketPage.js
+++ src/components/ViewSingleBasketPage.js
@@ -7,35 +7,7 @@ import { Box, Link, Button } from '@mui/material'
 import Copyright from './Copyright';
 import BasketCard from './BasketCard';
 export default function ViewSingleBasketPage() {
-    
-
   const lastBasketData = useSelector(state => state.user.lastBasketData)
-  console.log(lastBasketData)
-
-  // const updateCard = (basketData) => {
-  //   const card = ( 
-  //     <React.Fragment>
-  //       <><CardContent>
-  //          <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-  //            {basketData.basketNameStr}
-  //          </Typography>
-  //          <Typography variant="h5" component="div">
-
-  //            {/* Return: {percentReturn.toString().slice(0,5)}% */}
-  //          </Typography>
-  //          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-  //            {/* Present basket value = ${presentBasketValue.toLocaleString(undefined, { maximumFractionDigits: 2 })} */}
-  //            <br></br>
-  //            {/* Basket value on {indexDate} = ${initialBasketValue.toLocaleString(undefined, { maximumFractionDigits: 2 })} */}
-  //          </Typography>
-  //          <Typography>
-  //          </Typography>
-  //        </CardContent></>
-  //     </React.Fragment>
-  //   );
-  //   return card
-  //   }
-
   return (
     <div> 
       <div>
