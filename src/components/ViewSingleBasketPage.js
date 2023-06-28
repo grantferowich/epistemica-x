e0b6564da9d37371e69485340b4846692bd39fd0
grantferowich <g.ferowich@gmail.com>
2023-06-28 12:22:07 -0500

    optimized loc

--- src/components/ViewSingleBasketPage.js
+++ src/components/ViewSingleBasketPage.js
@@ -5,56 +5,49 @@ import CardContent from "@mui/material/CardContent";
 import { Typography } from '@mui/material';
 import { Box, Link, Button } from '@mui/material'
 import Copyright from './Copyright';
+import BasketCard from './BasketCard';
 export default function ViewSingleBasketPage() {
     
 
   const lastBasketData = useSelector(state => state.user.lastBasketData)
   console.log(lastBasketData)
 
-  const updateCard = (basketData) => {
-    const card = ( 
-      <React.Fragment>
-        <><CardContent>
-           <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-             {basketData.basketNameStr}
-           </Typography>
-           <Typography variant="h5" component="div">
-             {/* Return: {percentReturn.toString().slice(0,5)}% */}
-           </Typography>
-           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-             {/* Present basket value = ${presentBasketValue.toLocaleString(undefined, { maximumFractionDigits: 2 })} */}
-             <br></br>
-             {/* Basket value on {indexDate} = ${initialBasketValue.toLocaleString(undefined, { maximumFractionDigits: 2 })} */}
-           </Typography>
-           <Typography>
-           </Typography>
-         </CardContent></>
-      </React.Fragment>
-    );
-    return card
-    }
+  // const updateCard = (basketData) => {
+  //   const card = ( 
+  //     <React.Fragment>
+  //       <><CardContent>
+  //          <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+  //            {basketData.basketNameStr}
+  //          </Typography>
+  //          <Typography variant="h5" component="div">
 
-    // return (
-    //   <div> 
-    //     <div>
-    //       <h3>Here is the basket you just created!</h3>
-    //     </div>
-    // </div>)
+  //            {/* Return: {percentReturn.toString().slice(0,5)}% */}
+  //          </Typography>
+  //          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+  //            {/* Present basket value = ${presentBasketValue.toLocaleString(undefined, { maximumFractionDigits: 2 })} */}
+  //            <br></br>
+  //            {/* Basket value on {indexDate} = ${initialBasketValue.toLocaleString(undefined, { maximumFractionDigits: 2 })} */}
+  //          </Typography>
+  //          <Typography>
+  //          </Typography>
+  //        </CardContent></>
+  //     </React.Fragment>
+  //   );
+  //   return card
+  //   }
+
   return (
     <div> 
       <div>
         <h3>Here is the basket you just created!</h3>
       </div>
-  
+
       <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>      
         <div style={{ marginBottom: '600px'}}>
           <Box sx={{ maxWidth: '400px', width: '100%', margin: '0 auto' }}>
-            <Card>
-              {updateCard(lastBasketData)}
-            </Card>
+            <BasketCard basketHM={lastBasketData} />
           </Box>
         </div>
-      
         <Box sx={{ textAlign: 'center'}}>
           <div>
             <div>
